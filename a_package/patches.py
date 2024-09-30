7f2b84c6de16b73cbf242b7023d2e9c7434209a2
yizhen <yizhen@uni-book.local>
2024-09-30 12:12:58 +0200

    20240930 Patches fractal dimension by counting box

--- a_package/patches.py
+++ a_package/patches.py
@@ -1,13 +1,14 @@
 import numpy as np
 import numpy.linalg as la
-import scipy.optimize as optimize
+import matplotlib.pyplot as plt
+
 import SurfaceTopography.Uniform.GeometryAnalysis as ga
 
 
 def _borders(n: int):
-    """Indices to split an array of size `n` into 2, 4, 8, ..., k (2^(k+1) >= n) parts."""
-    n_sect = 2
-    indices = [0]
+    """Indices to split an array of size 'n' into 2, 4, 8, ..., k (2^{k+1} >= n) parts."""
+    n_sect = 2  # number of sections
+    indices = [0]  # indices of the border elements
     while n_sect < n:
         div = n // n_sect
         indices.extend([index + div for index in indices])
@@ -18,43 +19,58 @@ def _borders(n: int):
 
 def _count(blocks: list[np.ndarray]):
     """Count the number of blocks which are marked."""
-    return sum(np.logical_or.reduce(elem, axis=None) for elem in blocks)
+    return np.sum(np.logical_or.reduce(elem, axis=None) for elem in blocks)
 
 
 def box_count_dimension(mark: np.ndarray):
     """
-    :param mark: either boolean or non-negative integer
+    mark: all values are boolean
     """
     nx, ny = mark.shape
     assert nx == ny, "Only implemented for square-shape data"
 
-    size = []
-    num = []
+    box_side_len = []
+    fractal_area = []
+
     for ix in _borders(nx):
         rows = np.split(mark, ix, axis=0)
-        size.append(nx / len(ix))
-        num.append(sum(_count(np.split(row, ix, axis=1)) for row in rows))
+        num_pixels = nx / len(ix)
+        box_side_len.append(num_pixels)
+        fractal_area.append(sum(_count(np.split(row, ix, axis=1)) for row in rows))
 
-    return np.array(size), np.array(num)
+    # NOTE: don't use side length 1 because it cause problem in the weighting of least squares
+    # box_side_len.append(1)
+    # fractal_area.append(mark.sum())
+
+    return np.array(box_side_len), np.array(fractal_area)
 
 
-def least_squares(x: np.ndarray, y: np.ndarray):
-    """y = a x^b <=> logy = loga + b logx; parameter fitting with y = Phi theta formulation."""
-    # def err(p, x, y):
-    #     return np.log(y) - (np.log(p[0]) + p[1] * np.log(x))
-    # res = optimize.least_squares(err, [1, -1], args=(x, y))
-    # return res['x']
-    y = np.log2(y)
-    phi = np.column_stack([np.ones_like(x), np.log2(x)])
-    theta = la.pinv(phi) @ y
-    theta[0] = np.exp2(theta[0])
-    return theta
+def fit_linear_least_squares(y: np.ndarray, Phi: np.ndarray):
+    """Parameter fitting with y = Phi theta formulation."""
+    return la.pinv(Phi) @ y  # theta = (Phi)^(-1) y with pseudo-inverse
+
+
+def fit_power_curve(l_box: np.ndarray, n_box: np.ndarray, is_area: bool):
+    # Use 'log' to convert power into linear
+    # N = a L^b <=> log(N) = log(a) + b log(L) <=> log(N) = [1 log(L)] [log(a) b]^T
+    y = np.log2(n_box)
+    Phi = np.column_stack([np.ones_like(l_box), np.log2(l_box)])
+    # Add weights. Because the larger the box is, the less accurate the count is.
+    w = np.log2(l_box)**(-1)
+    if is_area:
+        w = w**2
+    print(f"weighting={w}")
+    # Least-squares fitting
+    log_a, b = fit_linear_least_squares(w*y, w[:,np.newaxis]*Phi)
+    a = np.exp2(log_a)
+    return a, b
 
 
 def gridwise_area_dimension(patch: np.ndarray):
     l_box, n_box = box_count_dimension(patch)
-    pref, slope = least_squares(l_box**2, n_box)
-    return pref, 1 - slope
+    pref, slope = fit_power_curve(l_box, n_box, is_area=True)
+    dimension = -slope
+    return dimension
 
 
 def gridwise_perimeter_dimension(patch: np.ndarray, outer: bool=True):
@@ -64,23 +80,36 @@ def gridwise_perimeter_dimension(patch: np.ndarray, outer: bool=True):
         peri = ga.inner_perimeter_area(patch, True)
 
     l_box, n_box = box_count_dimension(peri)
-    pref, slope = least_squares(l_box, n_box)
-    return pref, 1 - slope
+    pref, slope = fit_power_curve(l_box, n_box, is_area=False)
+    dimension = -slope
+    return dimension
 
 
-def patchwise_area_dimension(patch: np.ndarray):
-    n_patch, patch = ga.assign_patch_numbers_area(patch, True)
-    dims = np.empty((n_patch, 2))
-    for i_patch in np.arange(n_patch):
-        mark = patch == i_patch + 1  # the non-patch area is marked with 0; first index for patches is 1
-        dims[i_patch] = gridwise_area_dimension(mark)
-    return dims
+def plot_patches_dimension_fitting(ax: plt.Axes, l_box: np.ndarray, n_box: np.ndarray,
+                                   pref: float, slope: float):
+    ax.plot(l_box, n_box, 'rx', label="Box counting")
+    l_axis = np.linspace(l_box.min(), l_box.max(), num=200)
+    ax.plot(l_axis, pref*(l_axis**slope), 'b--', label="Fitting")
+
+    ax.set_xlabel(r"Side length of the box $L$ (#pixel)")
+    ax.set_ylabel(r"Dimension $N$")
+    ax.loglog()
 
 
-def patchwise_perimeter_dimension(patch: np.ndarray):
-    n_patch, patch = ga.assign_patch_numbers_area(patch, True)
-    dims = np.empty((n_patch, 2))
-    for i_patch in np.arange(n_patch):
-        mark = patch == i_patch + 1  # the non-patch area is marked with 0; first index for patches is 1
-        dims[i_patch] = gridwise_perimeter_dimension(mark)
-    return dims
+# TODO: instead use area / area plot.
+# def patchwise_area_dimension(patch: np.ndarray):
+#     n_patch, patch = ga.assign_patch_numbers_area(patch, True)
+#     dims = np.empty((n_patch, 2))
+#     for i_patch in np.arange(n_patch):
+#         mark = patch == i_patch + 1  # the non-patch area is marked with 0; first index for patches is 1
+#         dims[i_patch] = gridwise_area_dimension(mark)
+#     return dims
+
+
+# def patchwise_perimeter_dimension(patch: np.ndarray):
+#     n_patch, patch = ga.assign_patch_numbers_area(patch, True)
+#     dims = np.empty((n_patch, 2))
+#     for i_patch in np.arange(n_patch):
+#         mark = patch == i_patch + 1  # the non-patch area is marked with 0; first index for patches is 1
+#         dims[i_patch] = gridwise_perimeter_dimension(mark)
+#     return dims
---
+++ test/test_patches.py
@@ -0,0 +1,69 @@
+import numpy as np
+import numpy.random as random
+import matplotlib.pyplot as plt
+
+from a_package.patches import *
+
+
+show_me_plots = True
+_rng = random.default_rng()
+
+
+def test_area_fractal_dimension():
+    # Generate a square patch
+    n_pixel = 4000
+    square = random_square_patch(n_pixel)
+
+    # Curve fitting to find the dimension
+    l_box, n_box = box_count_dimension(square)
+    pref, slope = fit_power_curve(l_box, n_box, is_area=True)
+    dim = -slope
+    print(f"Dimension={dim}")
+
+    if show_me_plots:
+        fig, ax = plt.subplots()
+        plot_patches_dimension_fitting(ax, l_box, n_box, pref, slope)
+        fig.tight_layout()
+        plt.show()
+
+    # Compare to reference value
+    ref_val = 2.0
+    tol = 0.05
+    assert np.isclose(dim, ref_val, atol=tol), \
+        f"The value of area fractal dimension is not in the range of {ref_val:.2f}+-{tol:.2f}."
+
+
+def test_perimeter_fractal_dimension():
+    # Generate a square patch
+    n_pixel = 5000
+    square = random_square_patch(n_pixel)
+
+    # Get the perimeter
+    square_border =  ga.outer_perimeter_area(square, periodic=True)
+
+    # Get the dimension via curve fittingd
+    l_box, n_box = box_count_dimension(square_border)
+    pref, slope = fit_power_curve(l_box, n_box, is_area=False)
+    dim = -slope
+    print(f"Dimension={dim}")
+
+    if show_me_plots:
+        fig, ax = plt.subplots()
+        plot_patches_dimension_fitting(ax, l_box, n_box, pref, slope)
+        fig.tight_layout()
+        plt.show()
+
+    # Compare to reference value
+    ref_val = 1.0
+    tol = 0.02
+    assert np.isclose(dim, ref_val, atol=tol), \
+        f"The value of area fractal dimension is not in the range of {ref_val:.2f}+-{tol:.2f}."
+
+
+def random_square_patch(n_pixel: int):
+    """Generate a square randomly located in a 'n_pixel' by 'n_pixel' region."""
+    square = np.zeros((n_pixel, n_pixel), dtype=bool)
+    i_min = int(_rng.random() * 0.5 * n_pixel)
+    i_max = i_min + n_pixel//4
+    square[i_min:i_max, i_min:i_max] = 1
+    return square
