c3638af8e8001490630af5a7d6db335f356c8f71
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2025-08-12 19:41:40 +0200

    ENH: add a new sim case

---
+++ cases/sinusoid-over-flat/script.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import configparser
+
+import numpy as np
+import numpy.random as random
+import matplotlib.pyplot as plt
+
+from a_package.modelling import Region, CapillaryBridge
+from a_package.solving import AugmentedLagrangian
+from a_package.storing import working_directory
+from a_package.routine import simulate_quasi_static_pull_push
+
+
+show_me = False
+
+# define the working path by file name
+from utils.common import get_runtime_dir
+case_name = os.path.basename(os.path.dirname(__file__))
+working_dir = get_runtime_dir(case_name)
+
+
+def main():
+    config = configparser.ConfigParser()
+    for file in sys.argv[1:]:
+        if not os.path.exists(file):
+            raise RuntimeError(f"Non-existing file {file}")
+        print(f"Reading config from {file}")
+        config.read(file)
+
+    a = config['Grid'].getfloat('pixel_size')
+    N = config['Grid'].getint('nb_pixels')
+    L = a * N
+    region = Region(a, L, L, N, N)
+
+    # height profile of sinusoid
+    wave_num = config['Surface'].getint('wavenumber')
+    wave_amp = config['Surface'].getfloat('amplitude')
+    xm = region.xm
+    qx = (2*np.pi / L) * wave_num
+    ym = region.ym
+    qy = (2*np.pi / L) * wave_num
+    h1 = wave_amp * np.cos(qx * xm) * np.cos(qy * ym)
+
+    # # add a bit height variation
+    # variation_percent = 1e-2
+    # convexity = variation_percent / (0.5*L)**2
+    # h1 *= 1 + convexity * (xm - 0.5*L)**2 * (ym - 0.5*L)**2
+
+    # height profile of flat
+    h2 = np.zeros((N, N))
+
+
+    eta = config['Capillary'].getfloat('interface_thickness')
+    theta = config['Capillary'].getfloat('contact_angle_degree')
+    gamma = -np.cos(theta / 180.0 * np.pi)
+
+    # specify the trajectory
+    d_min = config['Trajectory'].getfloat('min_separation')
+    d_max = config['Trajectory'].getfloat('max_separation')
+    d_step = config['Trajectory'].getfloat('step_size')
+
+    # combine into the model object
+    capi = CapillaryBridge(region, eta, gamma, h1, h2)
+
+    # set liquid volume
+    capi.z1 = d_min
+    capi.update_gap()
+    capi.phi = np.ones((N, N))
+    capi.update_phase_field()
+    V_percent = 0.01 * config['Capillary'].getfloat('liquid_volume_percent')
+    V = capi.volume * V_percent
+
+    # solving parameters
+    i_max = config['Solver'].getint('max_nb_iters')
+    l_max = config['Solver'].getint('max_nb_loops')
+    tol_conver = config['Solver'].getfloat('tol_convergence')
+    tol_constr = config['Solver'].getfloat('tol_constraints')
+    c_init = config['Solver'].getfloat('init_penalty_weight')
+    solver = AugmentedLagrangian(i_max, l_max, tol_conver, tol_constr, c_init)
+
+    # Visual check before running
+    if show_me:
+        fig, ax = plt.subplots()
+        # image = ax.pcolormesh(region.xm/a, region.ym/a, h1/a, cmap='hot')
+        image = ax.imshow(h1/a, interpolation='bicubic', cmap='plasma', extent=[0, N, 0, N])
+        fig.colorbar(image)
+
+        fig, ax = plt.subplots()
+        # image = ax.pcolormesh(region.xm/a, region.ym/a, gap/a, cmap='hot')
+        image = ax.imshow(capi.g/a, interpolation='bicubic', cmap='hot', extent=[0, N, 0, N])
+        fig.colorbar(image)
+
+        plt.show()
+
+    # run the sim
+    with working_directory(working_dir, read_only=False) as store:
+        store.brand_new()
+        # random initial guess
+        capi.phi = random.rand(N, N) 
+        simulate_quasi_static_pull_push(store, capi, solver, V, d_min, d_max, d_step)
+
+
+if __name__ == '__main__':
+    main()
