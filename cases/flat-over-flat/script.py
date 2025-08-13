a95dea9c950b4226b284e17734cc368bf689eb63
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2025-08-13 20:39:35 +0200

    MAINT: unify main() strucutre among simulation scripts

--- cases/flat-over-flat/script.py
+++ cases/flat-over-flat/script.py
@@ -3,6 +3,7 @@ import sys
 
 import numpy as np
 import numpy.random as random
+import matplotlib.pyplot as plt
 
 from a_package.modelling import Region, CapillaryBridge
 from a_package.solving import AugmentedLagrangian
@@ -11,15 +12,18 @@ from a_package.routine import simulate_quasi_static_pull_push
 
 from utils.common import get_runtime_dir, read_configs
 
+
+show_me = False
+
+# define the working path by file name
 case_name = os.path.basename(os.path.dirname(__file__))
 working_dir = get_runtime_dir(case_name)
-show_me = False
 
 
 def main():
     config = read_configs(sys.argv[1:])
 
-    # Grid
+    # grid
     a = config['Grid'].getfloat('pixel_size')
     N = config['Grid'].getint('nb_pixels')
     L = a * N
@@ -29,20 +33,18 @@ def main():
     h1 = np.zeros([N, N])
     h2 = np.zeros([N, N])
 
-    # trajectory of mean separation
+    # specify the trajectory
     d_min = config['Trajectory'].getfloat('min_separation')
     d_max = config['Trajectory'].getfloat('max_separation')
     d_step = config['Trajectory'].getfloat('step_size')
 
-    # primary parameters
+    # create the capillary model
     eta = config['Capillary'].getfloat('interface_thickness')
     theta = config['Capillary'].getfloat('contact_angle_degree')
     gamma = -np.cos(theta / 180 * np.pi)
-
-    # combine into the model object
     capi = CapillaryBridge(region, eta, gamma, h1, h2)
 
-    # set liquid volume
+    # specify liquid volume by a percentage
     capi.z1 = d_min
     capi.update_gap()
     capi.phi = np.ones([N, N])
@@ -58,10 +60,29 @@ def main():
     c_init = config['Solver'].getfloat('init_penalty_weight')
     solver = AugmentedLagrangian(i_max, l_max, tol_conver, tol_constr, c_init)
 
+    # Visual check before running
+    if show_me:
+        fig, ax = plt.subplots()
+        # image = ax.pcolormesh(region.xm/a, region.ym/a, h1/a, cmap='hot')
+        image = ax.imshow(h1/a, interpolation='bicubic', cmap='plasma', extent=[0, N, 0, N])
+        fig.colorbar(image)
+
+        fig, ax = plt.subplots()
+        # image = ax.pcolormesh(region.xm/a, region.ym/a, gap/a, cmap='hot')
+        image = ax.imshow(capi.g/a, vmin=0, interpolation='bicubic', cmap='hot', extent=[0, N, 0, N])
+        fig.colorbar(image)
+
+        plt.show()
+        skip = input("Run simulation [Y/n]? ")[:1].upper() == "N"
+        if skip:
+            sys.exit(0)
+
     # run simulation routine
     with working_directory(working_dir, read_only=False) as store:
+        # clean the working dir
         store.brand_new()
-        # random initial guess
+
+        # start sim with random initial guess
         capi.phi = random.rand(N, N)
         capi.update_phase_field()
         simulate_quasi_static_pull_push(store, capi, solver, V, d_min, d_max, d_step)
