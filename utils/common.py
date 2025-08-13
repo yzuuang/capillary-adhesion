2f6adbb07f063937c80c2e0565340ae0d44f18fa
Yizhen Wang IMTEK <yizhen.wang@imtek.uni-freiburg.de>
2025-08-13 20:46:17 +0200

    Revert "MAINT: use a runtime directory to dump intermediate results"
    This reverts commit bd91c96ef08809bca57aaa1ab0a214384c6309e4.

--- utils/common.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-repo_root = os.getenv('PYTHONPATH')
-
-def get_runtime_dir(name: str):
-    """A dedicated directory to dump outputs on-the-fly."""
-    return os.path.join(repo_root, 'runtime', name)
