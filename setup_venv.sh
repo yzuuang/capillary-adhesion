f4261ff5dec09fba347d03926c944fbe8652e251
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2025-08-15 15:53:41 +0200

    MAINT: soften specification of python version; comments.

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
-# get dir name
+# if no automation, source it manually
 if ! command -v direnv; then
     source .envrc  &>/dev/null
 fi
+
+# get dir name
 directory=${VIRTUAL_ENV}
 
 # overwrite if one virtual env already exist
 if [ -d "${directory}" ]; then
+    echo "Delete the existing ${directory}."
     rm -rf "${directory}"
 fi
-python3.11 -m venv "${directory}"
+
+# use the same python version if available
+preferred_python="python3.11"
+if ! command -v ${preferred_python}; then
+    preferred_python="python"
+fi
+
+# create virtual environment
+echo "Create venv at ${directory}."
+${preferred_python} -m venv "${directory}"
 
 # install packages
 source "${directory}/bin/activate"
