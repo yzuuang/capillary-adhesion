FIX: get forces numerically cause phase-field has an implicit dependency

a_package/routine.py
@@ -39,12 +39,11 @@ def post_process(res: SimulationResult):
     t = np.empty(n_step)
     g = []
     phi = []
-    r = np.empty((n_step, n_dimension))
-    F = np.empty((n_step, n_dimension))
-    E = np.empty((n_step))
-    p = np.empty((n_step))
-    V = np.empty((n_step))               # volume
-    P = np.empty((n_step))               # perimeter
+    r = np.empty((n_step, n_dimension))  
+    E = np.empty((n_step))               
+    p = np.empty((n_step))               
+    V = np.empty((n_step))               
+    P = np.empty((n_step))               
 
     # use the model for computing extra quantities
     capi = res.modelling
@@ -63,15 +62,17 @@ def post_process(res: SimulationResult):
         phi.append(capi.phi)
 
         r[i] = capi.displacement
-        F[i] = capi.force
 
         E[i] = capi.energy
         p[i] = step.lam
         V[i] = capi.volume
         P[i] = capi.perimeter
 
+    # get normal force by numerical differences
+    Fz = (E[1:] - E[:-1]) / (r[1:, 2] - r[:-1, 2])
+
     # pack in an object
-    evo = Evolution(t, g, phi, r, F, E, p, V)
+    evo = Evolution(t, g, phi, r, E, p, V, P, Fz)
     return ProcessedResult(res.modelling, res.solving, evo)
 
 
@@ -80,11 +81,12 @@ class Evolution:
     t_exec: np.ndarray
     g: list[np.ndarray]
     phi: list[np.ndarray]
-    r: np.ndarray
-    F: np.ndarray
-    E: np.ndarray
-    p: np.ndarray
-    V: np.ndarray
+    r: np.ndarray       # relative displacement
+    E: np.ndarray       # energy
+    p: np.ndarray       # presure
+    V: np.ndarray       # volume
+    P: np.ndarray       # perimeter
+    Fz: np.ndarray      # normal force
 
 
 @dc.dataclass

a_package/visualizing.py
@@ -242,7 +242,7 @@ def plot_normal_force(ax: plt.Axes, pr: ProcessedResult, n_step: int=None):
         n_step = len(pr.evolution.t_exec)
 
     # Get the first few data points
-    f = pr.evolution.F[:n_step]
+    f = pr.evolution.Fz[:n_step-1]
 
     # Non-dimensionalize
     a = pr.modelling.region.a
@@ -250,33 +250,54 @@ def plot_normal_force(ax: plt.Axes, pr: ProcessedResult, n_step: int=None):
 
     # Plot the x, y, z component of forces
     steps = np.arange(n_step)
-    ax.plot(steps, f[:,2], color="b", linestyle="-", marker="o", ms=3, mfc="none", label=r"$F_z$")
+    steps = (steps[1:] + steps[:-1]) / 2
+    ax.plot(steps, f, color="b", linestyle="-", marker="o", ms=3, mfc="none", label=r"$F_z$")
 
     # Format the plot
     ax.legend(loc='upper right')
     ax.grid()
 
 
-def plot_shear_force(ax: plt.Axes, pr: ProcessedResult, n_step: int=None):
+def plot_perimeter(ax: plt.Axes, pr: ProcessedResult, n_step: int=None):
 
     if n_step is None:
         n_step = len(pr.evolution.t_exec)
 
     # Get the first few data points
-    f = pr.evolution.F[:n_step]
+    P = pr.evolution.P[:n_step]
 
     # Non-dimensionalize
     a = pr.modelling.region.a
-    f = f / a  # NOTE: actually needs to be divided by 'eta gamma', but 'gamma' is symbolic so far.
 
     # Plot the x, y, z component of forces
     steps = np.arange(n_step)
-    ax.plot(steps, f[:,0], color="r", linestyle="-", marker="o", ms=5, mfc="none", label=r"$F_x$")
-    ax.plot(steps, f[:,1], color="g", linestyle="--", marker="^", ms=5, mfc="none", label=r"$F_y$")
+    ax.plot(steps, P/a, color="r", linestyle="-", marker="o", ms=5, mfc="none", label=r"$P$")
 
     # Format the plot
     ax.legend(loc='upper right')
     ax.grid()
+
+
+# def plot_shear_force(ax: plt.Axes, pr: ProcessedResult, n_step: int=None):
+
+#     if n_step is None:
+#         n_step = len(pr.evolution.t_exec)
+
+#     # Get the first few data points
+#     f = pr.evolution.F[:n_step]
+
+#     # Non-dimensionalize
+#     a = pr.modelling.region.a
+#     f = f / a  # NOTE: actually needs to be divided by 'eta gamma', but 'gamma' is symbolic so far.
+
+#     # Plot the x, y, z component of forces
+#     steps = np.arange(n_step)
+#     ax.plot(steps, f[:,0], color="r", linestyle="-", marker="o", ms=5, mfc="none", label=r"$F_x$")
+#     ax.plot(steps, f[:,1], color="g", linestyle="--", marker="^", ms=5, mfc="none", label=r"$F_y$")
+
+#     # Format the plot
+#     ax.legend(loc='upper right')
+#     ax.grid()
 
 
 def plot_force_curves(ax: plt.Axes, pr: ProcessedResult, idx_stop: int=None):