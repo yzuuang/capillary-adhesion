FIX: get forces numerically cause phase-field has an implicit dependency

a_package/modelling.py
@@ -305,33 +305,33 @@ class ComputeCapillary:
 
         return perimeter_integrand.sum() * self.element_area
 
-    def compute_force(self):
-        # update necessary power terms
-        self.phi_powers[1] = self.phi_powers[0] ** 2
-        self.phi_powers[2] = self.phi_powers[0] * self.phi_powers[1]
-        self.phi_powers[3] = self.phi_powers[1] ** 2
+    # def compute_force(self):
+    #     # update necessary power terms
+    #     self.phi_powers[1] = self.phi_powers[0] ** 2
+    #     self.phi_powers[2] = self.phi_powers[0] * self.phi_powers[1]
+    #     self.phi_powers[3] = self.phi_powers[1] ** 2
 
-        # the common part of 3 components
-        # constant within the element: eta (dphi_dx^2 + dphi_dy^2)
-        square_grad = self.eta * (self.dphi_dx ** 2 + self.dphi_dy ** 2)
-        # compute at quadrature points: (1/eta)(phi^2 - 2 phi^3 + phi^4)
-        double_well = (1 / self.eta) * (self.phi_powers[1] - 2 * self.phi_powers[2] + self.phi_powers[3])
-        de_dg = (double_well + square_grad)
+    #     # the common part of 3 components
+    #     # constant within the element: eta (dphi_dx^2 + dphi_dy^2)
+    #     square_grad = self.eta * (self.dphi_dx ** 2 + self.dphi_dy ** 2)
+    #     # compute at quadrature points: (1/eta)(phi^2 - 2 phi^3 + phi^4)
+    #     double_well = (1 / self.eta) * (self.phi_powers[1] - 2 * self.phi_powers[2] + self.phi_powers[3])
+    #     de_dg = (double_well + square_grad)
 
-        # the different part of 3 components
-        f_x = (de_dg * self.dg_dx).sum()
-        f_y = (de_dg * self.dg_dy).sum()
-        # dg_dz = 1
-        f_z = de_dg.sum()
+    #     # the different part of 3 components
+    #     f_x = (de_dg * self.dg_dx).sum()
+    #     f_y = (de_dg * self.dg_dy).sum()
+    #     # dg_dz = 1
+    #     f_z = de_dg.sum()
 
-        # TODO: add contribution of water-solid interface
-        # h1_common = np.pow(self.dh1_dx**2 + self.dh1_dy**2 + 1, -1/2)
-        # water_h1_jacobian = (h1_common * self.dh1_dx) @ self.map.Dx + (h1_common * self.dh1_dy) @ self.map.Dy
-        # h2_common = np.pow(self.dh2_dx**2 + self.dh2_dy**2 + 1, -1/2)
-        # water_h2_jacobian = (h2_common * self.dh2_dx) @ self.map.Dx + (h2_common * self.dh2_dy) @ self.map.Dy
-        # area_water_solid_jacobian = self.beta * (water_h1_jacobian + water_h2_jacobian)
+    #     # TODO: add contribution of water-solid interface
+    #     # h1_common = np.pow(self.dh1_dx**2 + self.dh1_dy**2 + 1, -1/2)
+    #     # water_h1_jacobian = (h1_common * self.dh1_dx) @ self.map.Dx + (h1_common * self.dh1_dy) @ self.map.Dy
+    #     # h2_common = np.pow(self.dh2_dx**2 + self.dh2_dy**2 + 1, -1/2)
+    #     # water_h2_jacobian = (h2_common * self.dh2_dx) @ self.map.Dx + (h2_common * self.dh2_dy) @ self.map.Dy
+    #     # area_water_solid_jacobian = self.beta * (water_h1_jacobian + water_h2_jacobian)
 
-        return np.array([f_x, f_y, f_z]) * self.element_area
+    #     return np.array([f_x, f_y, f_z]) * self.element_area
 
     def compute_energy_jacobian(self) -> np.ndarray:
         # update necessary power terms

utils/overview.py
@@ -73,17 +73,17 @@ def animate_droplet_evolution_with_curves(pr: ProcessedResult):
     e_min = e_min - e_margin
     e_max = e_max + e_margin
 
-    F_n_min = np.amin(pr.evolution.F[:, -1]) / a
-    F_n_max = np.amax(pr.evolution.F[:, -1]) / a
+    F_n_min = np.amin(pr.evolution.Fz) / a
+    F_n_max = np.amax(pr.evolution.Fz) / a
     F_n_margin = view_margin_scale * (F_n_max - F_n_min)
     F_n_min = F_n_min - F_n_margin
     F_n_max = F_n_max + F_n_margin
 
-    F_t_min = np.amin(pr.evolution.F[:, 0:2]) / a
-    F_t_max = np.amax(pr.evolution.F[:, 0:2]) / a
-    F_t_margin = view_margin_scale * (F_t_max - F_t_min)
-    F_t_min = F_t_min - F_t_margin
-    F_t_max = F_t_max + F_t_margin
+    P_min = np.amin(pr.evolution.P) / a
+    P_max = np.amax(pr.evolution.P) / a
+    P_margin = view_margin_scale * (P_max - P_min)
+    P_min = P_min - P_margin
+    P_max = P_max + P_margin
 
     def update_image(i_frame: int):
         for ax in axs:
@@ -92,16 +92,15 @@ def animate_droplet_evolution_with_curves(pr: ProcessedResult):
         plot_gibbs_free_energy(axs[0], pr, i_frame + 1)
         axs[0].set_ylim(e_min, e_max)
         axs[0].set_ylabel(r"Energy $E/\gamma_\mathrm{lv} a^2$")
-        axs[0].set_title("Energy evolution")
+        axs[0].set_title("Evolution")
 
         plot_normal_force(axs[1], pr, i_frame + 1)
         axs[1].set_ylim(F_n_min, F_n_max)
-        axs[1].set_ylabel(r"Force $F/\gamma_\mathrm{lv} a$")
-        axs[1].set_title("Force evolution")
+        axs[1].set_ylabel(r"Normal force $F/\gamma_\mathrm{lv} a$")
 
-        plot_shear_force(axs[2], pr, i_frame + 1)
-        axs[2].set_ylim(F_t_min, F_t_max)
-        axs[2].set_ylabel(r"Force $F/\gamma_\mathrm{lv} a$")
+        plot_perimeter(axs[2], pr, i_frame + 1)
+        axs[2].set_ylim(P_min, P_max)
+        axs[2].set_ylabel(r"Perimeter $P/a$")
 
         axs[2].set_xlim([0, n_step])
         axs[2].set_xlabel(r"Step (size=$0.1 a$)")