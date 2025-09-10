8ded969cff288dc5350800a428af5ceabde1d730
yizhen <yizhen.wang@imtek.uni-freiburg.de>
2025-09-10 16:32:59 +0200

    MAINT: extract sweep to a separate util, add doc strings.

--- a_package/utils/__init__.py
+++ a_package/utils/__init__.py
@@ -1,3 +1,4 @@
 from .logging import reset_logging, switch_log_file
 from .runtime import register_run, retrieve_run
 from .data_io import working_directory, FilesToRead, FilesToReadWrite
+from .sweep import Sweep
